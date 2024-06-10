003b11327c724e91d0a9120fecf1de93cf58b4b8
Dylan <dylanps4495@gmail.com>
2024-06-09 21:25:15 -0400

    - Made imports (BeautifulSoup for Web Scraping, requests for fact making)
    - Created and stubbed necessary information finding and fact compiling methods

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 """
 Project: Fun Facts with Animals
 File: main.py
@@ -10,13 +13,36 @@ Author: Dylan Sturr
 """
 
 
+def get_info(inpt):
+    """
+    Determines if the input is an animal or not. If so, scrapes the necessary info from various sources.
+    Utilizes BeautifulSoup.
+    :param inpt: (possible) animal input
+    :return: whether the input is an animal or not
+    """
+    return False
+
+
+def get_facts(source):
+    """
+    Given the source material, compiles a list of 5 interesting facts about that animal.
+    :param source: source to compile facts from
+    :return: the list of facts
+    """
+    facts = []
+    return facts
+
+
 def main():
     """
     Gathers animal input and calls helper methods to ultimately achieve this project's goal
     :return:
     """
     animal_name = input("Name any animal: ")
-    print(animal_name)
+    if get_info(input):
+        print(animal_name)
+    else:
+        print(f"Invalid input: {animal_name} is not a recognized animal name!")
 
 
 if __name__ == '__main__':
