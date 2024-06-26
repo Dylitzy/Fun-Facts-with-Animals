2d35f13373b6941f88b3974700c60435a6416074
Dylan <dylanps4495@gmail.com>
2024-06-25 20:28:40 -0400

    - Created new method query which checks for valid animal input and returns a list of animals based on the given input
    - get_info method will instead find the best sources for interesting facts about the chosen animal

--- main.py
+++ main.py
@@ -14,19 +14,30 @@ Author: Dylan Sturr
 """
 
 
-def get_info(inpt):
+def query(inpt):
     """
-    Determines if the input is an animal or not. If so, scrapes the necessary info from various sources.
-    Utilizes BeautifulSoup.
+    Determines if the input is an animal or not. If so, returns a list of results based on the animal list.
     :param inpt: (possible) animal input
-    :return: whether the input is an animal or not
+    :return:
     """
+    results = []
     with open("animals.txt") as animals:
         for animal in animals:
             stripped = animal.strip()  # don't think about this too much
-            if inpt.title() == stripped:
-                return True
-    return False
+            if inpt.title() in stripped:
+                results.append(stripped)
+    return results
+
+
+def get_info(animal):
+    """
+    Given the picked animal, finds the top 5 best sources of information about that animal and returns
+    them in a list
+    :param animal: animal input
+    :return: the list of sources
+    """
+    sources = []
+    return sources
 
 
 def get_facts(source):
@@ -45,8 +56,16 @@ def main():
     :return:
     """
     animal_name = input("Name any animal: ")
-    if get_info(animal_name):
-        print(f"{animal_name.title()} is an animal!")
+    results = query(animal_name)
+    if len(results) != 0:
+        print("Found", len(results), "matches:")
+        for i in range(len(results)):
+            print(f"{i + 1}. {results[i]}")
+        animal_name = input("\nSelect an animal from the list above: ")
+        if animal_name in results:
+            print("Valid! Have a good day! (WIP)")
+        else:
+            print(f"Invalid input: {animal_name} was not found among the filtered results. Please check your spelling.")
     else:
         print(f"Invalid input: {animal_name} is not a recognized animal name! Please check your spelling.")
 
